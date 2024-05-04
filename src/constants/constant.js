8d5374721bdf411f1e0a6b494b897fef689b16cc
vignesh <vigneshfornavy@gmail.com>
2024-05-04 10:57:02 +0530

    Navbar Heading Change

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,6 +1,6 @@
 export const fetchList = "https://jsonplaceholder.typicode.com/todos";
 export const headTitle = "Add Your Task!";
-export const navHead = "MAKE A NOTE";
+export const navHead = "RIDDEV-TASK";
 export const pending =" 🟡Pending";
 export const complted = "🟢Finished"
 export const deleteIcon = "❌";
